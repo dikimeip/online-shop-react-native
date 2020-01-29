9b759bf09e8d2c48768435409acd20d348148110
dikimeip <pm159145@gmail.com>
2020-01-29 08:43:55 +0700

    slacing dasar menu account page

--- src/containers/pages/AccountPage/index.js
+++ src/containers/pages/AccountPage/index.js
@@ -5,8 +5,10 @@ import { View, Text } from 'react-native';
 class AccountPage extends Component{
     render(){
         return(
-            <View>
-                <Text>AKUN PAGE</Text>
+            <View style={{flex:1}}>
+               <View style={{height:40,backgroundColor:'black'}}></View>
+               <View style={{flex:1,backgroundColor:'blue'}}></View>
+               <View style={{height:65,backgroundColor:'red'}}></View>
             </View>
         )
     }
