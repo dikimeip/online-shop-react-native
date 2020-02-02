af569cbc76f80d25e7a85dee7c1f431f0515d06f
dikimeip <pm159145@gmail.com>
2020-02-02 10:54:59 +0700

    membuat fungsi untuk menampilkan data hot produk

--- src/configs/axios/index.js
+++ src/configs/axios/index.js
@@ -12,10 +12,23 @@ const POST = (path,data) => {
     return promise
 }
 
+const GET_ID = (path) => {
+    const promise = new Promise((reject,resolve) => {
+        Axios.get(RootPath+path).then(res => {
+            resolve(res.data)
+        }).catch(err => {
+            reject(err)
+        })
+    })
+    return promise
+}
+
 const PostLogin = (data) => POST('LoginController',data)
+const GetHotProduk = () => GET_ID('ProdukAll?id=HOTPRODUK')
 
 const API = {
-    PostLogin
+    PostLogin,
+    GetHotProduk,
 }
 
 export default API
