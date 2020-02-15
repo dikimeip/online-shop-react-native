f051fba156aed789547d537a0939718c04c849f3
dikimeip <pm159145@gmail.com>
2020-02-15 11:06:06 +0700

    menampilkan detail data produk peakaian anak

--- src/containers/organism/AnakProduk/index.js
+++ src/containers/organism/AnakProduk/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import API from '../../../configs/axios';
 import { View } from 'react-native';
 import ProdukItem from '../../../components/moleculs/ProdukItem';
+import { withNavigation } from 'react-navigation';
 
 class AnakProduk extends Component {
     constructor(props) {
@@ -10,6 +11,17 @@ class AnakProduk extends Component {
             produk: [],
             url: "http://192.168.43.230/server_dc-shop/assets/img/"
         }
+    }
+
+    pindahDetail = (id, nama, harga, deskripsi, stok, foto) => {
+        this.props.navigation.navigate('detailPages', {
+            id: id,
+            nama: nama,
+            harga: harga,
+            deskripsi: deskripsi,
+            stok: stok,
+            foto: foto
+        })
     }
 
     componentDidMount = () => {
@@ -24,7 +36,9 @@ class AnakProduk extends Component {
 
     render() {
         const produk = this.state.produk.map(pro => (
-            <ProdukItem key={pro.id_produk} image={{uri : this.state.url + pro.image_produk}} nama={pro.nama_produk} harga={pro.harga} />
+            <ProdukItem key={pro.id_produk} image={{uri : this.state.url + pro.image_produk}} nama={pro.nama_produk} harga={pro.harga}
+            tekan={() => this.pindahDetail(pro.id_produk, pro.nama_produk, pro.harga, pro.deskripsi, pro.stok, pro.image_produk)}
+            />
         ))
         return (
             <View style={{ flex: 1, paddingHorizontal: 20, flexDirection: 'row', flexWrap: 'wrap' }}>
@@ -34,4 +48,4 @@ class AnakProduk extends Component {
     }
 }
 
-export default AnakProduk
+export default withNavigation(AnakProduk) 
