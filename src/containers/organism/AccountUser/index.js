95f70a0fec1f849f4a11c5a74dcf527765c6bb79
dikimeip <pm159145@gmail.com>
2020-02-22 09:34:49 +0700

    show detail user login

--- src/containers/organism/AccountUser/index.js
+++ src/containers/organism/AccountUser/index.js
@@ -1,32 +1,86 @@
-import React from 'react'
-import { View,ScrollView,Image,Text } from 'react-native'
+import React, { Component } from 'react';
+import { View, ScrollView, Image, Text } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
+import API from '../../../configs/axios';
+import AsyncStorage from '@react-native-community/async-storage';
+import Loading from 'react-native-whc-loading';
 
-const AccountUser = (props) => {
-    return (
-        <View style={{ flex: 1, backgroundColor: '#A8E3A0' }}>
-            <ScrollView>
-                <View style={{ height: 500, backgroundColor: 'white', margin: 10, justifyContent: 'center' }}>
-                    <View style={{ height: 100, alignItems: 'center' }}>
-                        <Image source={require('../../../assets/img/user.png')} style={{ width: '20%', height: '100%' }} />
+
+class AccountUser extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            id: "",
+            username: "",
+            email: "",
+            no: "",
+            alamat: ""
+        }
+    }
+
+    getData = async () => {
+        try {
+            const myId = await AsyncStorage.getItem('user');
+            if (myId !== null) {
+                console.log("My Id " + myId)
+                this.setState({
+                    id: myId
+                })
+            } else {
+                console.log('Id Kosong')
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    componentDidMount = () => {
+        this.getData()
+        this.refs.loading.show()
+        setTimeout(() => {
+            API.GetUserId(this.state.id).then(res => {
+                console.log(res.value)
+                this.setState({
+                    username: res.value[0].username,
+                    email: res.value[0].email,
+                    no: res.value[0].no_hp,
+                    alamat: res.value[0].alamat
+                })
+                this.refs.loading.close()
+            }).catch(er => {
+                console.log(er)
+            })
+        }, 100);
+    }
+
+
+    render() {
+        return (
+            <View style={{ flex: 1, backgroundColor: '#A8E3A0' }}>
+                <ScrollView>
+                    <View style={{ height: 500, backgroundColor: 'white', margin: 10, justifyContent: 'center' }}>
+                        <View style={{ height: 100, alignItems: 'center' }}>
+                            <Image source={require('../../../assets/img/user.png')} style={{ width: '20%', height: '100%' }} />
+                        </View>
+                        <View style={{ marginTop: 20, marginHorizontal: 20 }}>
+                            <Text style={{ fontSize: 20, fontWeight: 'bold', marginTop: 10 }}>USERNAME</Text>
+                            <Text style={{ fontSize: 20 }}>{this.state.username}</Text>
+                            <Text style={{ fontSize: 20, fontWeight: 'bold', marginTop: 10 }}>EMAIL</Text>
+                            <Text style={{ fontSize: 20 }}>{this.state.email}</Text>
+                            <Text style={{ fontSize: 20, fontWeight: 'bold', marginTop: 10 }}>NO TELPON</Text>
+                            <Text style={{ fontSize: 20 }}>{this.state.no}</Text>
+                            <Text style={{ fontSize: 20, fontWeight: 'bold', marginTop: 10 }}>ALAMAT</Text>
+                            <Text style={{ fontSize: 20 }}>{this.state.alamat}</Text>
+                            <TouchableOpacity style={{ padding: 10, backgroundColor: 'green', borderRadius: 15, marginTop: 20 }}>
+                                <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'white', textAlign: 'center' }}>EDIT ACOUNT</Text>
+                            </TouchableOpacity>
+                        </View>
                     </View>
-                    <View style={{ marginTop: 20, marginHorizontal: 20 }}>
-                        <Text style={{ fontSize: 20, fontWeight: 'bold', marginTop: 10 }}>USERNAME</Text>
-                        <Text style={{ fontSize: 20 }}>{props.uname}</Text>
-                        <Text style={{ fontSize: 20, fontWeight: 'bold', marginTop: 10 }}>EMAIL</Text>
-                        <Text style={{ fontSize: 20 }}>{props.email}</Text>
-                        <Text style={{ fontSize: 20, fontWeight: 'bold', marginTop: 10 }}>NO TELPON</Text>
-                        <Text style={{ fontSize: 20 }}>{props.no}</Text>
-                        <Text style={{ fontSize: 20, fontWeight: 'bold', marginTop: 10 }}>ALAMAT</Text>
-                        <Text style={{ fontSize: 20 }}>{props.alamat}</Text>
-                        <TouchableOpacity style={{padding: 10,backgroundColor:'green',borderRadius:15,marginTop:20}}>
-                            <Text style={{fontSize:20,fontWeight:'bold',color:'white',textAlign:'center'}}>EDIT ACOUNT</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </ScrollView>
-        </View>
-    )
+                </ScrollView>
+                <Loading ref="loading" />
+            </View>
+        )
+    }
 }
 
 export default AccountUser
--- src/containers/pages/AccountPage/index.js
+++ src/containers/pages/AccountPage/index.js
@@ -3,61 +3,14 @@ import { View, } from 'react-native';
 import HeaderAccount from '../../organism/HeaderAccount';
 import Navbar from '../../organism/Navbar'
 import AccountUser from '../../organism/AccountUser';
-import AsyncStorage from '@react-native-community/async-storage';
-import API from '../../../configs/axios';
-
 
 class AccountPage extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            id: "",
-            username: "",
-            email: "",
-            no: "",
-            alamat: ""
-        }
-    }
-
-    getData = async () => {
-        try {
-            const myId = await AsyncStorage.getItem('user');
-            if (myId !== null) {
-                console.log("My Id " + myId)
-                this.setState({
-                    id: myId
-                })
-            } else {
-                console.log('Id Kosong')
-            }
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    componentDidMount = () => {
-        this.getData()
-        setTimeout(() => {
-            API.GetUserId(this.state.id).then(res => {
-                console.log(res.value[0].username)
-                this.setState({
-                    username: res.value[0].username,
-                    email: res.value[0].email,
-                    no: res.value[0].no_hp,
-                    alamat: res.value[0].alamat
-                })
-            }).catch(er => {
-                console.log(er)
-            })
-        }, 0);
-
-    }
     render() {
         console.log(this.state)
         return (
             <View style={{ flex: 1 }}>
                 {/* header  */}
-                <HeaderAccount uname={this.state.username} />
+                <HeaderAccount/>
                 {/* konten  */}
                 <AccountUser />
                 {/* navbar  */}
